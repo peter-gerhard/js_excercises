2d2ceee05fd82f85d9e72f7ba6b48c89ed43a998
Peter Gerhard <peter.gerhard@commercetools.de>
2018-12-05 00:55:48 +0100

    fix ticker

--- news-ticker/ticker.js
+++ news-ticker/ticker.js
@@ -1,32 +1,68 @@
-var box = document.getElementById("headlines");
+var headlines = document.getElementById("headlines");
 var links = document.getElementsByTagName("a");
-var left = box.offsetLeft;
+var firstLink = links[0];
+var position = headlines.offsetLeft;
 var reqId;
-var stop;
 
-function animate() {
-    left--;
+function moveHeadlines() {
+    position--;
+    headlines.style.left = position + "px";
+}
+
+function isOutOfView(link) {
+    return (position == -link.offsetWidth);
+}
+
+function resetPosition() {
+    position = 0;
+}
+
+function appendFirstLinkAtTheEnd() {
+    headlines.removeChild(firstLink);
+    headlines.appendChild(firstLink);
+}
+
+function determineNewFirstLink() {
+    firstLink = links[0];
+}
+
+function continueAnimation() {
     reqId = requestAnimationFrame(animate);
-    if (left <= -links[0].offsetWidth) {
-        left += links[0].offsetWidth;
-        box.appendChild(links[0]);
-    }
+}
 
-    box.style.left = left + "px";
+/*
+ * function for the animation
+ */
+function animate() {
+    if (isOutOfView(firstLink)) {
+        resetPosition();
+        appendFirstLinkAtTheEnd();
+        determineNewFirstLink();
+    }
+    moveHeadlines();
+    continueAnimation();
+}
+
+/*
+ * function to add the mouse over listeners
+ */
+function addMouseOverListeners() {
+    for (var i = 0; i < links.length; i++) {
+        const link = links[i];
+
+        link.addEventListener("mouseover", function (e) {
+            e.target.style.color = "blue";
+            e.target.style.textDecoration = "none";
+            cancelAnimationFrame(reqId);
+        });
+
+        link.addEventListener("mouseout", function (e) {
+            e.target.style.color = "purple";
+            e.target.style.textDecoration = "underline";
+            continueAnimation();
+        });
+    }
 }
 
 animate();
-
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("mouseover", function sty(e) {
-        // console.log("anything",links);
-        e.target.style.color = "blue";
-        e.target.style.fontStyle = "underline";
-
-        cancelAnimationFrame(reqId);
-    });
-
-    links[i].addEventListener("mouseout", function() {
-        animate();
-    });
-}
+addMouseOverListeners();
